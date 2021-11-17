2de808474c2d25cd5e507d68b65e465177368ee3
Pavel Prudnikov <pavelprudnikov2012@yandex.ru>
2021-11-16 23:52:35 -0800

    practise 3 tasks done

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -17,9 +17,13 @@ from django.urls import path
 
 from MainApp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page),
     path('snippets/list', views.snippets_page),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
